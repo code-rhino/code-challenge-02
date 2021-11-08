f630b2143eac538b80ee74061133e51a52d6328b
Eboni Coger <ebonicoger@Ebonis-MacBook-Air.local>
2021-11-08 14:07:39 -0500

    initial push

--- src/main/java/com/codedifferently/CountDupLetters.java
+++ src/main/java/com/codedifferently/CountDupLetters.java
@@ -1,13 +1,25 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDupLetters {
     /**
-     *
-     * @param input the string to be searched
+     * @param input  the string to be searched
      * @param letter the letter that you will search for
      * @return
      */
-    public Integer countDuplicateLetters(String input, String letter){
-        return null;
+
+    public Integer countDuplicateLetters(String input, String letter) {
+        Map<Character, Integer> duplicateLetters = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        Character oneLetter;
+        int count = 1;
+        for(int c = 0; c< input.length(); c++) {
+            oneLetter = input.charAt(c);
+            duplicateLetters.put(oneLetter,count);
+        }
+        System.out.println(duplicateLetters);
+        return count;
     }
 }
