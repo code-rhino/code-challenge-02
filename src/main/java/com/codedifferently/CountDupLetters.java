05288e11fb1c14ab3f3057dd8df62f24e07bc39d
Besrat Ghebrat <besratghebrat@Besrats-MacBook-Air.local>
2021-11-08 14:09:33 -0500

    submission

--- src/main/java/com/codedifferently/CountDupLetters.java
+++ src/main/java/com/codedifferently/CountDupLetters.java
@@ -1,5 +1,8 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDupLetters {
     /**
      *
@@ -7,7 +10,22 @@ public class CountDupLetters {
      * @param letter the letter that you will search for
      * @return
      */
+
+    //loop word
     public Integer countDuplicateLetters(String input, String letter){
+        Map<Character, Integer> dupLetters = new HashMap<>();
+        Character oneLetter;
+        input= input.toLowerCase();
+        int count = 1;
+        for(int c = 0; c< input.length(); c++){
+           oneLetter = input.charAt(c);
+           dupLetters.put(oneLetter,count);
+           if(dupLetters.containsKey(oneLetter)){
+               count = dupLetters.get(oneLetter);
+               count++;
+           } //getting outrageous counts
+        }
+        System.out.println(dupLetters);
         return null;
     }
 }
