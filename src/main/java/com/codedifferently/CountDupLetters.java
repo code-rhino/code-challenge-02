397b01bbf6674c6f5ff05fb68a4fba6220157044
Michiah <85957194+Michiah@users.noreply.github.com>
2021-11-08 14:07:01 -0500

    workin on it

--- src/main/java/com/codedifferently/CountDupLetters.java
+++ src/main/java/com/codedifferently/CountDupLetters.java
@@ -1,5 +1,8 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDupLetters {
     /**
      *
@@ -8,6 +11,19 @@ public class CountDupLetters {
      * @return
      */
     public Integer countDuplicateLetters(String input, String letter){
-        return null;
+        Integer count = 0;
+        Map<String, Integer> dupCount = new HashMap<>();
+        for (int i = 0; i < input.length(); i++){
+            if(input.equalsIgnoreCase(letter)){
+                dupCount.get(letter);
+                count++;
+
+                dupCount.put(letter,count);
+            }
+            else{
+                dupCount.put(letter,1);
+            }
+        }
+        return count;
     }
 }
