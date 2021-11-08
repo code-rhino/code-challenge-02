4f548857ba693ab7ce7eca239df21bbdf0d2bcff
Alvina Waseem <alvinawaseem@Alvinas-Air.fios-router.home>
2021-11-08 14:07:32 -0500

    still working

--- src/main/java/com/codedifferently/CountDupLetters.java
+++ src/main/java/com/codedifferently/CountDupLetters.java
@@ -1,5 +1,8 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDupLetters {
     /**
      *
@@ -7,7 +10,22 @@ public class CountDupLetters {
      * @param letter the letter that you will search for
      * @return
      */
-    public Integer countDuplicateLetters(String input, String letter){
-        return null;
+    public Integer countDuplicateLetters(String input, String letter) {
+        HashMap<String, Integer> charCountMap = new HashMap<String, Integer>();
+        String[] strArray = input.split("");
+        Integer count = 0;
+        for (String c : strArray) {
+            if (charCountMap.containsKey(c) && c.equalsIgnoreCase(letter)) {
+                count = charCountMap.get(c);
+                count++;
+                charCountMap.put(c, count);
+            } else {
+                charCountMap.put(c, 1);
+            }
+
+
+        }
+        return count;
     }
 }
+
