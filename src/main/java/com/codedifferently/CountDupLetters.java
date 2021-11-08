35e44a215efddff86eb68f84116a8cc941f0acd6
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-11-08 12:52:23 -0500

    added readme

--- src/main/java/com/codedifferently/CountDupLetters.java
+++ src/main/java/com/codedifferently/CountDupLetters.java
@@ -1,7 +1,12 @@
 package com.codedifferently;
 
 public class CountDupLetters {
-
+    /**
+     *
+     * @param input the string to be searched
+     * @param letter the letter that you will search for
+     * @return
+     */
     public Integer countDuplicateLetters(String input, String letter){
         return null;
     }
